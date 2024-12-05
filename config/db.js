513c8dad29cb7a37389fc7944510f28b267605c1
BrendaG06 <projetomedays@gmail.com>
2024-12-05 11:28:06 -0300

    adicionando o arquivo .env

--- config/db.js
+++ config/db.js
@@ -1,26 +1,32 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
+// Logs para depuração
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "******" : "undefined");
+console.log("DB_DATABASE:", process.env.DB_DATABASE);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,      
-    user: process.env.DB_USER,      
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,     
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST || "autorack.proxy.rlwy.net",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "caFeHODAgKaGcNMAXIrgAPVZfjoQKFPF",
+  database: process.env.DB_DATABASE || "railway",
+  port: process.env.DB_PORT || 36215,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err.message); 
-  } else {
-    console.log("Conexão bem-sucedida com o banco de dados!"); 
+(async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Conexão bem-sucedida com o banco de dados!");
     connection.release();
+  } catch (err) {
+    console.error("Erro ao conectar ao banco de dados:", err.message);
   }
-});
+})();
 
 module.exports = pool;
